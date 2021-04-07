e7109d1137e582de40758cd6099fecbbd9dc80cd
Poulomi Das <poulomidas976@gmail.com>
2021-04-06 20:55:21 -0700

    new changes

--- python_practice/knapsack_problem_dynammic_programming.py
+++ python_practice/knapsack_problem_dynammic_programming.py
@@ -2,24 +2,18 @@
 
 def findKnapSackDynammicProgramming(value, weight, totalWeight):
     # storage = [0] * [len(value) + 1][totalWeight + 1]
-    storage = [[0  for x in range(len(weight) + 1)]for x in range(totalWeight + 1)]
-    for i in range(len(value) + 1):
-        storage[i][0] = 0
-    for i in range(totalWeight + 1):
-        storage[0][i] = i
+    storage = [[0  for x in range(totalWeight + 1)]for x in range(len(value) + 1)]
 
-    for i in range(1 , len(value)):
-        for j in range(1, totalWeight + 1):
-            if(j - weight[i] > 0):
-                storage[i][j] = max(weight[i] + storage[i - 1][j - weight[i]], storage[i - 1][j])
+    for i in range(len(value) + 1):
+        for j in range(totalWeight + 1):
+            if(i == 0 or j == 0):
+                storage[i][j] = 0
+            if(j - weight[i - 1] >= 0):
+                storage[i][j] = max(value[i - 1] + storage[i - 1][j - weight[i - 1]], storage[i - 1][j])
             else:
                 storage[i][j] = storage[i - 1][j]
 
-    return storage[value][totalWeight]
-info@1stchoiceacupuncture.com
-
-
-
+    return storage[len(value)][totalWeight]
 
 
 
